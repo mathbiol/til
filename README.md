e30be9471615eb4f4433dc9942249c13ef835f1f
Jonas Almeida <jalmeida@mathbiol.org>
2018-04-16 08:52:23 -0400

    link to observable notebook

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # til
 Spatial Organization and Molecular Correlation of Tumor-Infiltrating Lymphocytes
+
+The core library is kept here, the interactive literate programming
+ interface interface that it enables is being developed as a 
+ [Observable Notebook](https://beta.observablehq.com/@jonasalmeida/til).
---
+++ til.js
@@ -0,0 +1,37 @@
+console.log('til.js loaded :-)')
+
+til=function(){
+    // ini
+    console.log('til initialized at'+Date())
+    til.loadTsv().then(function(tsv){
+        til.dt = til.dt||{}
+        til.all_indices_ap = til.tsv2tab(tsv)
+        console.log('done')
+    })
+}
+
+til.loadTsv = async function(url){
+    url = url||'all_indices_ap.csv'
+    var p = (await fetch(url)).text()
+    return p
+}
+
+til.tsv2tab = function(tsv){
+    var x = tsv.split(/[\n\r]/)
+    var parms = JSON.parse('['+x[0]+']')
+    var tab = {}
+    parms.forEach(function(p){
+        tab[p]=[]
+    })
+    x = x.slice(1).forEach(function(r){
+        r = r.replace(/"/g,'')
+        r.split(',').forEach(function(ri,i){
+            tab[parms[i]].push(ri)
+        })
+    })
+    return tab
+}
+
+
+
+//til()
